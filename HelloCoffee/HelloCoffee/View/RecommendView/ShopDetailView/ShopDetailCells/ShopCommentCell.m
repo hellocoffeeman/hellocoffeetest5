3aada9c2f0c2c7000cbc75200b56037141785619
hellocoffeeman <448048597@qq.com>
2014-12-17 18:46:02 +0800

    no message

--- HelloCoffee/HelloCoffee/View/RecommendView/ShopDetailView/ShopDetailCells/ShopCommentCell.m
+++ HelloCoffee/HelloCoffee/View/RecommendView/ShopDetailView/ShopDetailCells/ShopCommentCell.m
@@ -25,24 +25,26 @@
 -(void)layoutSubviewsFrame
 {
     // Cell 的宽高
-    CGFloat width = self.bounds.size.width;
-    CGFloat height = self.bounds.size.height;
-    NSLog(@"cell_w = %g ; cell_h = %g ",width,height);
+//    [self setNeedsUpdateConstraints];
+//    CGFloat width = self.bounds.size.width;
+//    CGFloat height = self.bounds.size.height;
+    NSLog(@"cell_w = %g ; cell_h = %g ",s_width,s_height);
     
     // 昵称 View [ h:28 ] [徽章紧跟在文字后]
-    self.nickNameView.frame = CGRectMake(30, 8, width-(30*2), 28);
+    self.nickNameView.frame = CGRectMake(30, 8, s_width-80-(30*2), 28);
     // 昵称 Label
-    CGSize nickNameSize = [self getNewSizeOfRectWithText:self.nickNamelabel.text limitSize:CGSizeMake(width-(30*2)-20, 28) fontSize:17.0];
-    self.nickNamelabel.frame = CGRectMake(((width-30*2)/2 - nickNameSize.width/2), 0, nickNameSize.width, nickNameSize.height);
+    CGSize nickNameSize = [self getNewSizeOfRectWithText:self.nickNamelabel.text limitSize:CGSizeMake(s_width-80-(30*2)-20, 28) fontSize:17.0];
+    self.nickNamelabel.frame = CGRectMake(((s_width-80-30*2)/2 - nickNameSize.width/2), 0, nickNameSize.width, nickNameSize.height);
     // 徽章 Img 14*16
-    self.badgeImageView.frame = CGRectMake((width-30*2)/2 + nickNameSize.width/2 + 4, 6, 14, 16);
+    self.badgeImageView.frame = CGRectMake((s_width-80-30*2)/2 + nickNameSize.width/2 + 4, 6, 14, 16);
     
     // 评论内容 Label - 差40 ？！因为XIB中Cellwidth = 320
-    CGSize contentSize = [self getNewSizeOfRectWithText:self.contentLabel.text limitSize:CGSizeMake(width-40, 52) fontSize:13.0];
-    self.contentLabel.frame = CGRectMake(0, 43, width-40, contentSize.height);
-    
+    CGSize contentSize = [self getNewSizeOfRectWithText:self.contentLabel.text limitSize:CGSizeMake(s_width-95, 52) fontSize:13.0];
+    NSLog(@"%f   height=%f",contentSize.width,contentSize.height);
+//    self.contentLabel.frame = CGRectMake(0, 43, s_width-80, contentSize.height);
+    self.contentLabel.frame = CGRectMake(0, 43, contentSize.width, contentSize.height);
     // 发表日期 Label
-    self.dateLabel.frame = CGRectMake(width-110-40, 43+contentSize.height + 1, 100, 18);
+    self.dateLabel.frame = CGRectMake(s_width-80-100-40, 43+contentSize.height + 1, 100, 18);
     
     
 }
